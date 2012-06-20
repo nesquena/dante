05447a5dc66e12591424ba84a9a304605c78b615
Ryan McKern <ryan@orangefort.com>
2012-06-20 17:04:46 -0400

    Cleaner shutdown message
    Rather than doing an error dump on Interrupt, dante
    now catches and handles the request gracefully.

--- lib/dante/runner.rb
+++ lib/dante/runner.rb
@@ -128,8 +128,12 @@ module Dante
     end
 
     def interrupt
-      raise Interrupt
-      sleep(1)
+      begin
+        raise Interrupt
+        sleep(1)
+      rescue Interrupt
+        log "Interrupt received; stopping #{@name}"
+      end
     end
 
     # Returns true if process is not running
